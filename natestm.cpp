d17748e5dcee5db8a12d082cff4315df2841ce3d
Nathan Jenkins <nate.jenkins@gmail.com>
2013-07-15 19:14:39 +0200

    linux almost working

---
+++ natestm.cpp
@@ -0,0 +1,591 @@
+
+#include <stdio.h>
+#include <string>
+#include "natestm.h"
+#include <GL/glui.h>
+//#include <complex.h>
+//#include <math.h>
+#include <complex>
+#include <cmath>
+#include <iostream>
+#include "define_constants.h"
+
+using namespace std;
+
+void draw(void);
+void drawTopo(const graphicsData& myGraphicsData, const ScanUserData& scanUserData);
+void drawSpectro(const graphicsData& myGraphicsData, const ScanUserData& scanUserData);
+void drawEcut(const graphicsData& myGraphicsData, const ScanUserData& scanUserData);
+
+void gluiCallback(int id);
+
+void idle(void);
+void allocateMemory(ScanUserData* scanUserData);
+void onResize(int width, int height);
+void onCalculateGap(int id);
+void onCalculateBand(int id);
+void onCalculateQuasi(int id);
+void onCalculateG011(int id);
+
+template <class T, class U>
+void dWaveGap(U gap0, T* kx, T* ky, T* gap, int rows){
+	int cols = rows;
+	int row, col;
+	for(row = 0; row<rows; row++){
+		for(col = 0; col<cols; col++){
+			gap[row*cols+col]= gap0*(cos(kx[col])-cos(ky[row]))/2.0;
+		}
+	}
+}
+
+template <class T, class U>
+void calcBandEnergy(T* kx, T* ky, U t1, U t2, U t3, U u, T* band, int rows){
+	int cols = rows;
+	int row, col;
+	for(row = 0; row<rows; row++){
+		for(col = 0; col<cols; col++){
+			band[row*cols+col]= 2.0*t1*(cos(kx[col])+cos(ky[row]))+
+								4.0*t2*(cos(kx[col])*cos(ky[row]))+
+								2.0*t3*(cos(2*kx[col])+cos(2*ky[row]))
+								- u;
+		}
+	}
+}
+
+template <class T>
+void calcQuasiEnergy(T* band, T* gap, T* quasi, int rows){
+	int cols = rows;
+	int row, col;
+	for(row = 0; row<rows; row++){
+		for(col = 0; col<cols; col++){
+			quasi[row*cols+col]= sqrt(	band[row*cols+col]*band[row*cols+col]+
+										gap[row*cols+col]*gap[row*cols+col]);
+		}
+	}
+}
+
+/*this functions calculates G11 for one energy, the next function for a range of energies*/
+template <class T>
+complex<T> calcG011(T w, complex<T> gamma, T* quasi_k, T* delta_k, int rows){
+	int cols = rows;
+	int row, col;
+	complex<T> t1, g011(0.0,0.0), wg;
+	wg = w + gamma;
+	for(row = 0; row<rows; row++){
+		for(col = 0; col<cols; col++){
+			t1 = pow(delta_k[row*cols+col], 2)/(wg + quasi_k[row*cols+col]);
+			
+			//g011 += 1.0/(w + gamma - quasi_k[row*cols+col] - t1);
+			g011 += 1.0/(wg - quasi_k[row*cols+col] - t1);
+		}
+	}
+	return g011;
+}
+
+template <class T>
+void calcG11(T wMax, int numSpecVoltages, complex<T> gamma, T* quasi_k, T* delta_k, complex<T>* G11, int rows){
+	
+	int i;
+	complex<T> t1;
+	T w, stepSize;
+	if(numSpecVoltages > 1)
+		stepSize = 2*wMax/(numSpecVoltages-1);
+	else 
+		stepSize = 0;
+	for(i=0;i<numSpecVoltages;i++){
+		w = -wMax + i*stepSize;
+		cout << "voltage is " << w << endl;
+		G11[i] = calcG011(w, gamma, quasi_k, delta_k, rows);
+	}
+}
+
+template <class T>
+T fermi(T energy, T fermiEnergy, T beta){
+	return 1/(pow(M_E, (energy-fermiEnergy)*beta) + 1.0);	
+	
+}
+
+
+
+
+template <class T>
+void arrayRange(T* array, T min, T max, int numSteps){
+	T stepSize = (max-min)/(numSteps-1);
+	for(int i=0; i<numSteps; i++){
+		array[i] = min + i*stepSize;
+	}
+}
+
+
+
+ScanUserData scanUserData;
+
+
+
+graphicsData myGraphicsData;
+
+
+void initGL(void){
+	glShadeModel(GL_SMOOTH);
+	float glcolors[4] = {-1,-1,-1,-1};
+	glClearColor(0,0.5, 0.6, 0);
+   
+   glGetFloatv(GL_COLOR_CLEAR_VALUE, glcolors);
+   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glClearDepth(2.0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	
+	
+	
+} 
+
+int main(int argc, char* argv[])
+{
+	
+	scanUserData.gaps = scanUserData.bandEnergy = scanUserData.quasiEnergy = 0;
+
+
+	scanUserData.quasiMin = scanUserData.bandMin = -1.0;
+	scanUserData.quasiMax = scanUserData.bandMax = 1.0;
+
+	scanUserData.t1 = -882;
+	scanUserData.t2 = 239;
+	scanUserData.t3 = -14;
+	scanUserData.Em = -26;
+	scanUserData.u  = -4.0*(scanUserData.t2 - scanUserData.t3) - scanUserData.Em;
+	scanUserData.vMax  = 100.0;
+	scanUserData.gamma = 1.0;
+
+	myGraphicsData.width = myGraphicsData.height = 700;
+	glutInitWindowSize(myGraphicsData.width,myGraphicsData.height);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	myGraphicsData.mainWindow = glutCreateWindow("natemodel");
+	glutDisplayFunc(draw);
+	glutReshapeFunc(onResize);
+	
+	initGL();
+	
+	
+
+	scanUserData.numSpecVoltages = 256;
+
+	scanUserData.nx = 256;
+	scanUserData.gap0 = 50;
+
+	scanUserData.action = TOPO_ACTION;
+
+	scanUserData.smoothSpecWidth = 3;
+
+	/****************************** USER INTERFACE *********************************************/
+
+	myGraphicsData.glui = GLUI_Master.create_glui( "GLUI", 0,myGraphicsData.width+20,0 );	
+
+	myGraphicsData.topoRollout = myGraphicsData.glui->add_rollout("Topo Params", true);
+	myGraphicsData.specRollout = myGraphicsData.glui->add_rollout("Spec Params", true);
+
+	/****************************** TOPOGRAPHY *********************************************/
+
+	myGraphicsData.nxSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "nx:", GLUI_SPINNER_INT, &(scanUserData.nx) );
+	myGraphicsData.nxSpinner->set_int_limits( NUM_XY_MIN, NUM_XY_MAX );
+
+	myGraphicsData.gapSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "gap0:", GLUI_SPINNER_FLOAT, &(scanUserData.gap0) );
+	myGraphicsData.gapSpinner->set_float_limits( 0.0, 100.0 );
+
+	myGraphicsData.t1Spinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "t1:", GLUI_SPINNER_FLOAT, &(scanUserData.t1) );
+	myGraphicsData.t1Spinner->set_float_limits( T_MIN, T_MAX );
+
+	myGraphicsData.t2Spinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "t2:", GLUI_SPINNER_FLOAT, &(scanUserData.t2) );
+	myGraphicsData.t2Spinner->set_float_limits( T_MIN, T_MAX );
+
+	myGraphicsData.t3Spinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "t3:", GLUI_SPINNER_FLOAT, &(scanUserData.t3) );
+	myGraphicsData.t3Spinner->set_float_limits( T_MIN, T_MAX );
+
+	myGraphicsData.EmSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "Em:", GLUI_SPINNER_FLOAT, &(scanUserData.Em) );
+	myGraphicsData.EmSpinner->set_float_limits( T_MIN, T_MAX );
+
+	myGraphicsData.uSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "u:", GLUI_SPINNER_FLOAT, &(scanUserData.u) );
+	myGraphicsData.uSpinner->set_float_limits( U_MIN, U_MAX );
+
+	myGraphicsData.vSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "vMax:", GLUI_SPINNER_FLOAT, &(scanUserData.vMax) );
+	myGraphicsData.vSpinner->set_float_limits( 0, V_MAX );
+
+	myGraphicsData.gammaSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "gamma:", GLUI_SPINNER_FLOAT, &(scanUserData.gamma) );
+	myGraphicsData.gammaSpinner->set_float_limits( GAMMA_MIN, GAMMA_MAX );
+
+	myGraphicsData.nvSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.topoRollout,  "num Vs:", GLUI_SPINNER_INT, &(scanUserData.numSpecVoltages) );
+	myGraphicsData.nvSpinner->set_int_limits( NUM_XY_MIN, NUM_XY_MAX );
+
+
+	myGraphicsData.glui->add_button_to_panel(myGraphicsData.topoRollout, "calculate Gap", -1, onCalculateGap);
+	myGraphicsData.glui->add_button_to_panel(myGraphicsData.topoRollout, "calculate Band", -1, onCalculateBand);
+	myGraphicsData.glui->add_button_to_panel(myGraphicsData.topoRollout, "calculate Quasi", -1, onCalculateQuasi);
+	myGraphicsData.glui->add_button_to_panel(myGraphicsData.topoRollout, "calculate G011", -1, onCalculateG011);
+
+	/****************************** SPECTROSCOPY *********************************************/
+	
+	myGraphicsData.vMinSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.specRollout, "vMin:", GLUI_SPINNER_FLOAT, &(scanUserData.vMin) );
+	myGraphicsData.vMinSpinner->set_int_limits( V_MIN, V_MAX );
+	
+	
+	myGraphicsData.glui->add_statictext_to_panel(myGraphicsData.specRollout, "         vMax == -vMin");
+
+	myGraphicsData.numSpecVoltagesSpinner = myGraphicsData.glui->add_spinner_to_panel(myGraphicsData.specRollout,"nv:", GLUI_SPINNER_INT, &(scanUserData.numSpecVoltages) );
+	myGraphicsData.numSpecVoltagesSpinner->set_int_limits( NUM_VOLTAGES_MIN, NUM_VOLTAGES_MAX );
+
+	//myGraphicsData.smoothSpecWidthSpinner->set_speed(SPINNER_SPEED);
+
+	myGraphicsData.glui->add_column(true);
+
+	myGraphicsData.glui->sync_live();
+	
+
+	myGraphicsData.autoScaleTopo = 1;
+	myGraphicsData.autoScaleSpec = 1;
+
+	//glutIdleFunc(idle);
+	GLUI_Master.set_glutIdleFunc(idle);
+
+	glutPostRedisplay();
+	glutMainLoop();
+
+
+//	delete[] myGraphicsData.dapQuestion;
+//	delete[] myGraphicsData.dapAnswer;
+// delete[] scanUserData.dapFileName;
+
+	return 0;
+}
+
+void idle(){
+	
+/*	scanUserData.topoMax  = getMax(scanUserData.topoData, scanUserData.numTopoPointsRead);
+	scanUserData.topoMin  = getMin(scanUserData.topoData, scanUserData.numTopoPointsRead);
+	scanUserData.topoMean = (double)getMean(scanUserData.topoData, scanUserData.numTopoPointsRead);
+	scanUserData.topoDev = sqrt(getVariance(scanUserData.topoData, scanUserData.numTopoPointsRead));
+	
+*/
+
+	sleep(1);
+
+//printf("idling\n");
+}
+
+void allocateMemory(ScanUserData* scanUserData){
+	printf("allocating scan memory\n");
+
+
+	if(scanUserData->gaps){
+		delete[] scanUserData->gaps;
+		scanUserData->gaps = 0;
+		
+	}
+
+	if(scanUserData->bandEnergy){
+		delete[] scanUserData->bandEnergy;
+		scanUserData->bandEnergy = 0;
+		
+	}
+
+	if(scanUserData->quasiEnergy){
+		delete[] scanUserData->quasiEnergy;
+		scanUserData->quasiEnergy = 0;
+		
+	}
+
+	if(scanUserData->kx){
+		delete[] scanUserData->kx;
+		scanUserData->kx = 0;
+		
+	}
+
+	if(scanUserData->ky){
+		delete[] scanUserData->ky;
+		scanUserData->ky = 0;
+		
+	}
+
+	if(scanUserData->G11){
+		delete[] scanUserData->G11;
+		scanUserData->G11 = 0;
+		
+	}
+
+	if(scanUserData->spec){
+		delete[] scanUserData->spec;
+		scanUserData->spec = 0;
+		
+	}
+
+	scanUserData->gaps = new double[scanUserData->nx*scanUserData->nx];
+	scanUserData->bandEnergy = new double[scanUserData->nx*scanUserData->nx];
+	scanUserData->quasiEnergy = new double[scanUserData->nx*scanUserData->nx];
+	scanUserData->kx = new double[scanUserData->nx];
+	scanUserData->ky = new double[scanUserData->nx];
+
+	scanUserData->G11 = new complex<double>[scanUserData->numSpecVoltages];
+	scanUserData->spec = new double[scanUserData->numSpecVoltages];
+
+
+	initArray(scanUserData->gaps, 0, scanUserData->nx*scanUserData->nx);
+	initArray(scanUserData->bandEnergy, 0, scanUserData->nx*scanUserData->nx);
+	initArray(scanUserData->quasiEnergy, 0, scanUserData->nx*scanUserData->nx);
+	initArray(scanUserData->kx, 0, scanUserData->nx);
+	initArray(scanUserData->ky, 0, scanUserData->nx);
+	initArray(scanUserData->G11, 0, scanUserData->numSpecVoltages);
+	initArray(scanUserData->spec, 0, scanUserData->numSpecVoltages);
+
+}
+
+template <class T>
+void displaySpecData(T *specData, int numPoints, double yMin, double yMax, double red, double green, double blue)
+{
+	//printf("display specdata\n");
+	int i;
+	//	double xScale, yScale, yOffset, delX;
+	
+	
+	if(specData==0)
+		return;
+	
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//set up an orthographic projection 
+	glOrtho(0, numPoints-1, yMin, yMax, -1, 1);
+	
+	//I draw the spectro graph in the forward and reverse directions
+	//because there is a problem in how this OpenGL implementation fills
+	//lines and a solution is to draw everything twice: once forward
+	//and once reverse.  Still beats using windows code.
+	glColor3f(red, green, blue);
+	glBegin(GL_LINE_STRIP);
+	//if(xStart <= xEnd){  //draw the spec without flipping the x axis
+	
+	for(i=0; i<numPoints; i++){
+		glVertex3f((float)i, (float)specData[i], 0.0f); 
+		//printf("i %d sd %f\n", i, (float)specData[i]*(float)yScale+(float)yOffset);
+		//glVertex3f(i, yMax/2.0, 0.0f);
+	}
+	glEnd();
+	
+	glBegin(GL_LINE_STRIP);
+	
+	
+	glEnd();
+}
+
+template <class T>
+void drawImage(T* image, int cols, int rows, double min, double max){
+	//printf("drawing\n");
+	int i,j;
+	float scale;
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//set up an orthographic projection 
+	glOrtho(0, cols, 0, rows, -1, 1);
+	//printf("width: %lf height: %lf\n", rows*curRange/range, rows*curRange/range);
+	
+	//lode the model matrix where we can rotate or translate our image
+	glMatrixMode(GL_MODELVIEW);
+	//clear the model matrix stack
+	glLoadIdentity(); 
+	
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_ALWAYS);
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	/*we don't have valid scan data*/
+	if(image == 0)
+		return;
+	
+	
+	if( (max-min) != 0.0)
+		scale = 1.0/(max-min);
+	else
+		scale = 1.0;
+	
+	glBegin(GL_QUADS);
+	
+	float color = 0;
+	
+	
+	for(i=0; i<rows; i++){
+		for(j=0; j<cols; j++){
+			color = scale*(image[i*cols+j]-min);
+			glColor3f(color, color, color);	
+			
+			glVertex3f(j, i, 0.0);
+			glVertex3f(j+1, i, 0.0);
+			glVertex3f(j+1, i+1, 0.0);
+			glVertex3f(j, i+1, 0.0);
+			
+			
+		}
+	}
+	
+	glEnd();
+	
+	
+	//glutSwapBuffers();
+	return;
+}
+
+void draw(void){
+//	printf("drawing\n");
+	glClearColor(0,0.5, 0.6, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	if(scanUserData.gaps){
+		glViewport(0,0, myGraphicsData.width/2.0, myGraphicsData.height/2.0);
+		drawImage(scanUserData.gaps, scanUserData.nx, scanUserData.nx, -scanUserData.gap0, scanUserData.gap0);
+	}
+	if(scanUserData.bandEnergy){
+		glViewport(myGraphicsData.width/2.0,0, myGraphicsData.width/2.0, myGraphicsData.height/2.0);
+		drawImage(scanUserData.bandEnergy, scanUserData.nx, scanUserData.nx, scanUserData.bandMin, scanUserData.bandMax);
+	}
+
+	if(scanUserData.quasiEnergy){
+		glViewport(myGraphicsData.width/2.0,myGraphicsData.height/2.0, myGraphicsData.width/2.0, myGraphicsData.height/2.0);
+		drawImage(scanUserData.quasiEnergy, scanUserData.nx, scanUserData.nx, scanUserData.quasiMin, scanUserData.quasiMax);
+	}
+
+	if(scanUserData.spec){
+		glViewport(0,myGraphicsData.height/2.0, myGraphicsData.width/2.0, myGraphicsData.height/2.0);
+		displaySpecData(scanUserData.spec, scanUserData.numSpecVoltages, scanUserData.specMin, scanUserData.specMax, 1.0,0.0,0.0);
+		//drawImage(scanUserData.quasiEnergy, scanUserData.nx, scanUserData.nx, scanUserData.quasiMin, scanUserData.quasiMax);
+	}
+
+	glutSwapBuffers();
+}
+
+void onResize(int width, int height){
+	myGraphicsData.width = width;
+	myGraphicsData.height = height;
+}
+
+void onCalculateGap(int id){
+	allocateMemory(&scanUserData);
+	arrayRange(scanUserData.kx, -PI, PI, scanUserData.nx);
+	arrayRange(scanUserData.ky, -PI, PI, scanUserData.nx);
+	dWaveGap(scanUserData.gap0, scanUserData.kx, scanUserData.ky, scanUserData.gaps, scanUserData.nx);
+
+
+	if(glutGetWindow() != myGraphicsData.mainWindow){
+				glutSetWindow(myGraphicsData.mainWindow);
+	}
+	glutPostRedisplay();
+}
+
+void onCalculateBand(int id){
+	allocateMemory(&scanUserData);
+	arrayRange(scanUserData.kx, -PI, PI, scanUserData.nx);
+	arrayRange(scanUserData.ky, -PI, PI, scanUserData.nx);
+	dWaveGap(scanUserData.gap0, scanUserData.kx, scanUserData.ky, scanUserData.gaps, scanUserData.nx);
+
+	scanUserData.u  = -4.0*(scanUserData.t2 - scanUserData.t3) - scanUserData.Em;
+	calcBandEnergy(	scanUserData.kx, scanUserData.ky, 
+					scanUserData.t1, scanUserData.t2, scanUserData.t3, 
+					scanUserData.u, scanUserData.bandEnergy, scanUserData.nx);
+
+	scanUserData.bandMin = getMin(scanUserData.bandEnergy, scanUserData.nx*scanUserData.nx);
+	scanUserData.bandMax = getMax(scanUserData.bandEnergy, scanUserData.nx*scanUserData.nx);
+
+	if(glutGetWindow() != myGraphicsData.mainWindow){
+				glutSetWindow(myGraphicsData.mainWindow);
+	}
+	glutPostRedisplay();
+
+}
+void onCalculateQuasi(int id){
+	allocateMemory(&scanUserData);
+	arrayRange(scanUserData.kx, -PI, PI, scanUserData.nx);
+	arrayRange(scanUserData.ky, -PI, PI, scanUserData.nx);
+	dWaveGap(scanUserData.gap0, scanUserData.kx, scanUserData.ky, scanUserData.gaps, scanUserData.nx);
+	scanUserData.u  = -4.0*(scanUserData.t2 - scanUserData.t3) - scanUserData.Em;
+	calcBandEnergy(	scanUserData.kx, scanUserData.ky, 
+					scanUserData.t1, scanUserData.t2, scanUserData.t3, 
+					scanUserData.u, scanUserData.bandEnergy, scanUserData.nx);
+
+	scanUserData.bandMin = getMin(scanUserData.bandEnergy, scanUserData.nx*scanUserData.nx);
+	scanUserData.bandMax = getMax(scanUserData.bandEnergy, scanUserData.nx*scanUserData.nx);
+
+	calcQuasiEnergy(scanUserData.bandEnergy, scanUserData.gaps, scanUserData.quasiEnergy, scanUserData.nx);
+	scanUserData.quasiMin = getMin(scanUserData.quasiEnergy, scanUserData.nx*scanUserData.nx);
+	scanUserData.quasiMax = getMax(scanUserData.quasiEnergy, scanUserData.nx*scanUserData.nx);
+
+	if(glutGetWindow() != myGraphicsData.mainWindow){
+				glutSetWindow(myGraphicsData.mainWindow);
+	}
+	glutPostRedisplay();
+
+}
+
+void onCalculateG011(int id){
+	allocateMemory(&scanUserData);
+	ScanUserData s;
+	s.kx=s.ky=s.gaps=s.quasiEnergy=s.bandEnergy=0;
+	s.spec = 0;
+	s.G11 = 0;
+	allocateMemory(&s);
+	arrayRange(scanUserData.kx, -PI, PI, scanUserData.nx);
+	arrayRange(scanUserData.ky, -PI, PI, scanUserData.nx);
+	dWaveGap(scanUserData.gap0, scanUserData.kx, scanUserData.ky, scanUserData.gaps, scanUserData.nx);
+	scanUserData.u  = -4.0*(scanUserData.t2 - scanUserData.t3) - scanUserData.Em;
+	calcBandEnergy(	scanUserData.kx, scanUserData.ky, 
+					scanUserData.t1, scanUserData.t2, scanUserData.t3, 
+					scanUserData.u, scanUserData.bandEnergy, scanUserData.nx);
+
+	scanUserData.bandMin = getMin(scanUserData.bandEnergy, scanUserData.nx*scanUserData.nx);
+	scanUserData.bandMax = getMax(scanUserData.bandEnergy, scanUserData.nx*scanUserData.nx);
+
+	calcQuasiEnergy(scanUserData.bandEnergy, scanUserData.gaps, scanUserData.quasiEnergy, scanUserData.nx);
+	scanUserData.quasiMin = getMin(scanUserData.quasiEnergy, scanUserData.nx*scanUserData.nx);
+	scanUserData.quasiMax = getMax(scanUserData.quasiEnergy, scanUserData.nx*scanUserData.nx);
+
+	complex<double> gamma(0.0,scanUserData.gamma);
+	double w = 0.0;
+
+	complex<double> g011;
+	//g011 = new complex<double>[scanUserData.nx*scanUserData.nx];
+
+	/*for(int i=0; i<10; i++){
+		g011 = calcG011((double)i, gamma, scanUserData.quasiEnergy, scanUserData.gaps, scanUserData.nx);
+		cout << i << endl;
+		cout << "re: " << g011.real() << " im: "<< g011.imag() << endl;
+	}*/
+
+	/*actually uses bare dispersion, not quasi dispersion*/
+	calcG11((double)scanUserData.vMax, scanUserData.numSpecVoltages, gamma, scanUserData.bandEnergy, scanUserData.gaps, scanUserData.G11, scanUserData.nx);
+	//calcG11((double)scanUserData.vMax, scanUserData.numSpecVoltages, gamma, scanUserData.quasiEnergy, scanUserData.gaps, scanUserData.G11, scanUserData.nx);
+
+	for(int i=0; i<scanUserData.numSpecVoltages; i++){
+		//cout << "re: " << scanUserData.G11[i].real() << " im: "<< scanUserData.G11[i].imag() << endl;
+		scanUserData.spec[i] = -(1/PI)*scanUserData.G11[i].imag();
+
+	}	
+
+	scanUserData.specMin = getMin(scanUserData.spec, scanUserData.numSpecVoltages);
+	scanUserData.specMax = getMax(scanUserData.spec, scanUserData.numSpecVoltages);
+	
+
+
+
+
+
+
+	if(glutGetWindow() != myGraphicsData.mainWindow){
+				glutSetWindow(myGraphicsData.mainWindow);
+	}
+	glutPostRedisplay();
+
+}
+	
+
+
+
---
+++ natestm.h
@@ -0,0 +1,260 @@
+#ifndef NATESTM_H
+#define NATESTM_H
+
+/*disable an annoying truncation warning*/
+#pragma warning(disable: 4786)
+#pragma warning(disable: 4275)
+#pragma warning(disable: 4251)
+#include <GL/glui.h>
+#include <complex>
+
+
+
+
+#define V_MIN -1000.0
+#define V_MAX 1000.0
+
+#define NUM_VOLTAGES_MIN 0
+#define NUM_VOLTAGES_MAX 1000
+#define NUM_XY_MIN 0
+#define NUM_XY_MAX 10000
+
+#define WAIT_SPINNER_SPEED 0.1
+#define SPINNER_SPEED 0.1
+
+#define SMOOTH_SPEC_MIN 0
+#define SMOOTH_SPEC_MAX 1000
+
+#define GAMMA_MAX 1000.0
+#define GAMMA_MIN 0.0001
+
+
+
+
+#define MOVE_ACTION 0
+#define TOPO_ACTION 1
+#define TOPO_AND_SPEC_ACTION 3
+#define SPEC_ACTION 2
+
+#define HEADER_SIZE 1000
+
+#define T_MIN -10000.0f
+#define T_MAX 10000.0f
+#define U_MIN -10000.0f
+#define U_MAX 10000.0f
+
+/*dap time is in microseconds, this is the multiplier we will use to convert between seconds*/
+
+#define PI 3.14159265
+
+struct imageScale{
+	double min;
+	double max;
+	double mean;
+	double dev;
+};
+
+std::complex<double> a;
+
+struct ScanUserData{
+
+	int numSpecVoltages, numSpecSamples, numSpec, nx, nv;
+
+	float vMin, vMax;
+	int	action;
+	
+	double* kx;
+	double* ky;
+	double* gaps;
+	double* bandEnergy;
+	double* quasiEnergy;
+	double* spec;
+	std::complex<double>* G11;
+
+
+	double bandMin;
+	double bandMax;
+	double quasiMin;
+	double quasiMax;
+	double specMin;
+	double specMax;
+	float gamma;
+
+	double GMin;
+	double GMax;
+
+	float t1, t2, t3, u, Em;
+
+	float gap0;
+
+	int smoothSpecWidth;
+
+
+	int specPixelSize;
+	int topoSize;
+	double specMapSize;
+
+
+	char* dapFileName;
+
+
+	int boolAutoSave;
+
+};
+
+/*graphics data holds values for the graphics, updateAfterAverages tells us how many times
+  to run the simulation before displaying on the screen, mainWindow is an ID for the
+  graphics library to know which window to draw in*/
+struct graphicsData{
+   int mainWindow;
+   GLUI *glui;
+
+	GLUI_Spinner *vMinSpinner;
+	GLUI_Spinner *vMaxSpinner;
+	GLUI_Spinner *numSpecVoltagesSpinner;
+
+	GLUI_Spinner *nxSpinner;
+	GLUI_Spinner *nySpinner;
+	GLUI_Spinner *gapSpinner;
+	GLUI_Spinner *t1Spinner;
+	GLUI_Spinner *t2Spinner;
+	GLUI_Spinner *t3Spinner;
+	GLUI_Spinner *uSpinner;
+	GLUI_Spinner *EmSpinner;
+	GLUI_Spinner *vSpinner;
+	GLUI_Spinner *gammaSpinner;
+	GLUI_Spinner *nvSpinner;
+
+	GLUI_Spinner *smoothSpecWidthSpinner;
+
+
+	GLUI_Button *moveButton;
+
+	GLUI_Rollout* topoRollout;
+	GLUI_Rollout* specRollout;
+
+	
+
+
+
+	int width;
+	int height;
+	int autoScaleTopo;
+	int autoScaleSpec;
+
+	int boolAutoScaleSpec;
+	int boolAutoScaleDeriv;
+	int boolAutoScaleTopo;
+	
+
+};
+
+template <class T>
+T getMax(T* array, int length){
+	if(length == 0)
+		return 0;
+	T max = array[0];
+	for(int i=0; i<length; i++){
+		if(array[i]>max)
+			max = array[i];
+	}
+	return max;
+}
+
+template <class T>
+T getMin(T* array, int length){
+	if(length == 0)
+		return 0;
+	T min = array[0];
+	for(int i=0; i<length; i++){
+		if(array[i]<min)
+			min = array[i];
+	}
+	return min;
+}
+
+template <class T>
+double getMean(T* array, int length){
+	if(length == 0)
+		return 0;
+	double mean = 0;
+	for(int i=0; i<length; i++){
+		mean += array[i];
+	}
+	return 1.0*mean/length;
+}
+
+template <class T>
+double getVariance(T* array, int length){
+	if(length == 0)
+		return 0;
+	double mean = getMean(array, length);
+	double var=0;
+	for(int i=0; i<length; i++){
+		var += (array[i]-mean)*(array[i]-mean);
+	}
+	return var/length;
+
+
+}
+
+template <class T>
+void deriv3(T* spec, double* derivSpec, int length){
+	for(int i=1; i<(length-1); i++){
+		derivSpec[i] = (spec[i+1]-spec[i-1])/2.0;
+	}
+	derivSpec[0] = spec[1]-spec[0];
+	derivSpec[length-1] = spec[length-1]-spec[length-2];
+}
+
+template <class T>
+void linsmooth(T* spec, double* smoothSpec, int halfWidth, int length){
+	int i,j,k;
+	double avg;
+
+	avg = 0;
+	for(j=0; j<=halfWidth; j++){
+		avg += spec[j];
+		
+	}
+	smoothSpec[0] = avg/j;
+
+	for(i=1; i<=halfWidth; i++){
+		avg += spec[j+i-1];
+		smoothSpec[i] = avg/(j+i);
+	}
+	for(i=halfWidth+1; i <(length-halfWidth); i++){
+	   avg += spec[i+halfWidth];
+	   avg -= spec[i-halfWidth-1];
+	   smoothSpec[i] = avg/(2*halfWidth+1);
+	}
+	k=1;
+	for(i=(length-halfWidth); i<length; i++){
+	   avg -= spec[i-halfWidth-1];
+      smoothSpec[i] = avg/(2*halfWidth+1-k);
+      k++;
+	}     
+}
+
+template <class T>
+void initArray(T* array, int initVal, int length){
+	if(length == 0)
+		return;
+	if(array == 0)
+		return;
+	for(int i=0; i<length; i++){
+		array[i]=0;
+	}
+}
+
+template <class T>
+void copyArray(T* array, T* arrayCopy, int length){
+	if(length == 0)
+		return;
+	for(int i=0; i<length; i++){
+		arrayCopy[i]=array[i];
+	}
+}
+
+
+#endif
